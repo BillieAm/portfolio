c46e90c27f2fab7f8d2bf14e0e24f620d7b0dcde
Billie Amit <biliamit@gmail.com>
2022-07-24 12:32:37 +0300

    add nav toggle full functioning and animation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
+const header = document.getElementById("header");
 const navToggle = document.getElementById("nav-toggle");
 const navTop = document.getElementById("nav-top");
+const navLinks = document.querySelectorAll(".nav-top__item");
 
 navToggle.addEventListener("click", function () {
-  navTop.classList.toggle("nav-top-open");
+  header.classList.toggle("nav-top-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    header.classList.remove("nav-top-open");
+  });
 });
